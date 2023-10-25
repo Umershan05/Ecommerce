50732a6023dbfc6944a0729f7dd0ee62f5806a9b
umershan05 <umershan07@gmail.com>
2023-10-25 10:38:13 -1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import Wishlist from './Pages/Wishlist';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header/>
+   <Routes>
+      <Route path='/'element={<Home/>}/>
+       <Route path='/Cart'element={<Cart/>}/>
+      <Route path='/Wishlist'element={<Wishlist/>}/>
+
       
-    </div>
+    </Routes>
+    <Footer/>
+    </>
   );
 }
 
